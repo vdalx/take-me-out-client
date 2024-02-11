8b4dd284cc93fa41aba298297c9e14ac3f08de48
vdalx <114463776+vdalx@users.noreply.github.com>
2024-02-11 14:28:32 -0500

    Added React router to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React from 'react';
 import './App.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import HomePage from './pages/HomePage';
+import CreateAccountPage from './pages/CreateAccountPage';
+import LoginPage from './pages/LoginPage';
+import AccountPage from './pages/AccountPage';
+import EventListingsPage from './pages/EventListingsPage';
+import EventPage from './pages/EventPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LandingPage/>} />
+        <Route path='/home' element={<HomePage/>} />
+        <Route path='/create-account' element={<CreateAccountPage/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/account' element={<AccountPage/>} />
+        <Route path='/event-list' element={<EventListingsPage/>} />
+        <Route path='/event' element={<EventPage/>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
