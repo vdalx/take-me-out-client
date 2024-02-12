8dc8fb0a2f97b3ef3273425d3a97d09d500ef224
vdalx <114463776+vdalx@users.noreply.github.com>
2024-02-11 22:38:53 -0500

    Completed initial building and styling of Landing Page and Landing Page components.

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -4,16 +4,28 @@ import PrimaryButton from '../../components/PrimaryButton';
 const LandingPage = () => {
     return (
         <div className='landing-page'>
-            <h1>Take Me Out</h1>
-            <div className='landing-page__btn-container'>
-                <PrimaryButton 
-                    btnText='log in'
-                    className='landing-page__btn--type-two'
-                />
-                <PrimaryButton 
-                    btnText='sign up'
-                    className='landing-page__btn--type-one'
-                />
+            <div className='landing-page__background-wrap'>
+                <div className='landing-page__content'>
+                    <div className='landing-page__title-wrap'>
+                        <h1 className='landing-page__title'>take me 
+                            <span className='landing-page__title--bold'> out</span>
+                        </h1>
+                    </div>
+                    <div className='landing-page__btn-container'>
+                        <div className='landing-page__btn'>
+                            <PrimaryButton 
+                                btnText='log in'
+                                className='landing-page__btn--type-two'
+                            />
+                        </div>
+                        <div className='landing-page__btn'>
+                            <PrimaryButton 
+                                btnText='sign up'
+                                className='landing-page__btn--type-one'
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
