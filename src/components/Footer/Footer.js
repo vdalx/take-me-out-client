be125af9fa702d5c077704cdd291e7e9a146aba4
vdalx <114463776+vdalx@users.noreply.github.com>
2024-02-17 17:15:05 -0500

    Added user profile and initial nav options to header in desktop view, added initial social media icons to footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,21 @@
 import './Footer.scss';
+import EmailIcon from '../../assets/icons/envelope-solid.svg';
+import FacebookIcon from '../../assets/icons/facebook.svg';
+import InstagramIcon from '../../assets/icons/instagram.svg';
+import TwitterIcon from '../../assets/icons/x-twitter.svg';
 
 const Footer = () => {
     return (
         <div className='footer'>
-            <h3 className='footer__text'>contact us</h3>
+            <div className='footer__container'>
+                <h3 className='footer__text'>contact us</h3>
+                <div className='footer__social-container'>
+                    <img className='footer__social-icon' src={EmailIcon} alt='Email icon showing an envelope'/>
+                    <img className='footer__social-icon' src={FacebookIcon} alt='Social media icon showing the Facebook logo'/>
+                    <img className='footer__social-icon' src={InstagramIcon} alt='Social media icon showing the Instagram logo'/>
+                    <img className='footer__social-icon' src={TwitterIcon} alt='Social media icon showing the Twitter logo'/>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/components/ProfileIcon/ProfileIcon.js
@@ -0,0 +1,15 @@
+import './ProfileIcon.scss';
+import UserAvatar from '../../assets/icons/user-solid.svg';
+
+const ProfileIcon = () => {
+    return (
+        <div className='profile-icon__container'>
+            <div className="profile-icon__img-container">
+                <img className="profile-icon__img" src={UserAvatar} alt='View of current user'/>
+            </div>
+            <p className='profile-icon__text'>profile</p>
+        </div>
+    )
+}
+
+export default ProfileIcon;
