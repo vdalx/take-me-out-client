66f39fabeda331318cfa6cd499a434c355b0eb7e
vdalx <114463776+vdalx@users.noreply.github.com>
2024-03-09 11:49:03 -0500

    Created initial component for progress bar - needs refining

--- src/components/EventCardSmall/EventCardSmall.js
+++ src/components/EventCardSmall/EventCardSmall.js
@@ -1,5 +1,6 @@
 import './EventCardSmall.scss';
 import EventImg from '../../assets/images/chris-zhang-uGh-hHVPRYI-unsplash.jpg';
+import ProgressBar from '../ProgressBar';
 
 const EventCardSmall = () => {
     return (
@@ -9,18 +10,20 @@ const EventCardSmall = () => {
                     <img className='event-card-small__hero-img' src={EventImg} alt='Venue of the event'/>
                 </div>
             </div>
-            <div className='event-card-small__event-info-container'>
-                <div className='event-card-small__event-details-container'>
-                    <h3 className='event-card-small__event-name'>an event</h3>
-                    <p className='event-card-small__event-venue'>massey hall</p>
-                    <p className='event-card-small__event-location'>Toronto</p>
-                </div>
-                <div className='event-card-small__event-date-container'>
-                    <p className='event-card-small__event-date'>sept 5 2024</p>
+            <div className='event-card-small__event-container'>
+                <div className='event-card-small__event-details'>
+                    <div className='event-card-small__event-info-container'>
+                        <h3 className='event-card-small__event-name'>an event</h3>
+                        <p className='event-card-small__event-venue'>massey hall</p>
+                        <p className='event-card-small__event-location'>Toronto</p>
+                    </div>
+                    <div className='event-card-small__event-date-container'>
+                        <p className='event-card-small__event-date'>Sept 5 2024</p>
+                    </div>
                 </div>
                 <div className='event-card-small__event-status-container'>
                     <div className='event-card-small__event-sellthru'>
-
+                        <ProgressBar/>
                     </div>
                     <div className='event-card-small__event-cost'>
 
