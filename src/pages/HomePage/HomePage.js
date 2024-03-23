624cc4759cf76f86e0719e4c43ce0e6146db8445
vdalx <114463776+vdalx@users.noreply.github.com>
2024-03-23 15:22:04 -0400

    Created initial components for content displayed on home page and will be referring to posts as articles

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,10 @@
 import './HomePage.scss';
+import ArticleListContainer from '../../components/ArticleListContainer';
 
 const HomePage = () => {
     return (
         <div className='home-page'>
-
+            <ArticleListContainer articleListTitle='What is happening?'/>
         </div>
     )
 }
