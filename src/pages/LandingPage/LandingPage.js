4dc03c7febcb1391d7ada595513c35c90ee521ef
vdalx <114463776+vdalx@users.noreply.github.com>
2024-05-05 21:14:51 -0400

    Added fully funtional hamburger menu with styling. It's not perfect, I still need to make some updates, but it's to an acceptable standard for the purpose of the project at this stage. Additional styling for fly in menu needed.

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -7,11 +7,6 @@ const LandingPage = () => {
         <div className='landing-page'>
             <div className='landing-page__background-wrap'>
                 <div className='landing-page__content'>
-                    <div className='landing-page__title-wrap'>
-                        <h1 className='landing-page__title'>take me 
-                            <span className='landing-page__title--bold'> out</span>
-                        </h1>
-                    </div>
                     <div className='landing-page__btn-container'>
                         <div className='landing-page__btn'>
                             <Link to='/login'>
