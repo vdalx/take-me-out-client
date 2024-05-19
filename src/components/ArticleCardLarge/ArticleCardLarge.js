e75a8ca392fd8bba3553d56d89f7e884ee11869c
vdalx <114463776+vdalx@users.noreply.github.com>
2024-05-19 01:34:11 -0400

    Made adjustments to styling for tablet view

--- src/components/ArticleCardLarge/ArticleCardLarge.js
+++ src/components/ArticleCardLarge/ArticleCardLarge.js
@@ -4,28 +4,30 @@ import EventCardSmall from '../EventCardSmall';
 const ArticleCardLarge = ({ post }) => {
     return(
         <div className='article-card-large'>
-            <div className='article-card-large__hero-img-container'>
-                <div>
+            <div className='article-card-large__hero-banner'>
+                <div className='article-card-large__hero-img-container'>
                     <img className='article-card-large__hero-img' src={post.post_image} alt='Venue of the event'/>
                 </div>
             </div>
-            <div className='article-card-large__content-container'>
-                <div className='article-card-large__title-container'>
-                    <h3 className='article-card-large__title'>{post.post_name}</h3>
+            <div className='article-card-large__container'>
+                <div className='article-card-large__content'>
+                    <div className='article-card-large__title-container'>
+                        <h2 className='article-card-large__title'>{post.post_name}</h2>
+                    </div>
+                    <div className='article-card-large__body-container'>
+                        <p className='article-card-large__body-container'>{post.post_body}</p>
+                    </div>
                 </div>
-                <div className='article-card-large__body-container'>
-                    <p className='article-card-large__body-container'>{post.post_body}</p>
+                <div className='event-list-container__card-wrap'>
+                    {post.post_events &&
+                        post.post_events.map(event =>
+                            <EventCardSmall
+                                key={event.id}
+                                event={event}
+                            />
+                        )
+                    }
                 </div>
-            </div>
-            <div className='event-list-container__card-wrap'>
-                {post.post_events &&
-                    post.post_events.map(event =>
-                        <EventCardSmall
-                            key={event.id}
-                            event={event}
-                        />
-                    )
-                }
             </div>
         </div>
     )
--- src/components/EventBadge/EventBadge.js
+++ src/components/EventBadge/EventBadge.js
@@ -1,6 +1,5 @@
 import './EventBadge.scss';
 import checkMark from '../../assets/icons/check-solid.svg';
-import dollarSign from '../../assets/icons/dollar-sign-solid-2.svg';
 import sterlingSign from '../../assets/icons/sterling-sign-solid-2.svg';
 
 const EventBadge = ({ badgeType, ticketPrice }) => {
@@ -19,7 +18,7 @@ const EventBadge = ({ badgeType, ticketPrice }) => {
         <div className='event-badge'>
             <div className='event-badge__container'>
                 <div className={`event-badge__img-container event-badge__img-container--${badgeType}`}>
-                    {badgeType === 'saved' || 'confirmed' ? 
+                    {badgeType === 'saved' ? 
                         <img className='event-badge__img' src={checkIconType()} alt='View of current user'/> :
                         <p className='event-badge__price'>{`£${ticketPrice}`}</p>
                     }
